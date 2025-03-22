6d08144aa7d9ceda80f2be7a2300821784de6f8c
agustinbutrico <agustinbutrico@gmail.com>
2025-03-22 04:56:11 -0300

    En data/load_data.py se creó la función load_obstacles() y se cambió la función load_gyms() por load_locations() así cargando todas las locaciones en una sola función.

--- data/load_data.py
+++ data/load_data.py
@@ -1,45 +1,50 @@
 import random
 
-import gyms.base as gyms
-import trainers.base as trainers
-from data import game_state, create_pokemons
+import data.game_state as gs
+import data.create_pokemons as cp
+import locations.base as loc
+from core import map
+from trainers.base import Trainer
 
-def load_gyms():
-    # Crear gimnasios y agregarlos al estado global
-    rock_gym = gyms.Gym(name="Gimnasio Roca", type_="Roca", icon="R")
-    water_gym = gyms.Gym(name="Gimnasio Agua", type_="Agua", icon="W")
-    electric_gym = gyms.Gym(name="Gimnasio Eléctrico", type_="Eléctrico", icon="E")
-    fighting_gym = gyms.Gym(name="Gimnasio Lucha", type_="Lucha", icon="F")
-    
-    new_gyms = [rock_gym, water_gym, electric_gym, fighting_gym]
+def load_obstacles():
+    map.obstacles_creation()
 
-    # Agregar todos los gimnacios al estado global
-    game_state.gyms.extend(new_gyms)
+def load_locations():
+    location_types = [
+        ("Rock Gym", " R", "Roca", loc.Gym),
+        ("Water Gym", " W", "Agua", loc.Gym),
+        ("Electric Gym", " E", "Electrico", loc.Gym),
+        ("Fighting Gym", " F", "Lucha", loc.Gym),
+        ("Hospital 1", " H", loc.Hospital),
+        ("Hospital 2", " H", loc.Hospital),
+    ]
+    map.locations_creation(location_types)
+    # Los constructores de Gym y Hospital agregan las instancias a game_state.gyms, game_state.hospitals y game_state.locations
 
 def load_trainers():
     # Crear entrenadores y asignarlos a sus respectivos gimnasios
-    trainer_user = trainers.Trainer("User")
-    trainer_brock = trainers.Trainer("Brock")
-    trainer_misty = trainers.Trainer("Misty")
-    trainer_ltsurge = trainers.Trainer("Lt. Surge")
-    trainer_sabrina = trainers.Trainer("Sabrina")
+    trainer_user = Trainer("User")
+    trainer_brock = Trainer("Brock")
+    trainer_misty = Trainer("Misty")
+    trainer_ltsurge = Trainer("Lt. Surge")
+    trainer_sabrina = Trainer("Sabrina")
     
     new_trainers = [trainer_brock, trainer_misty, trainer_ltsurge, trainer_sabrina]
     
     # Asignar entrenadores a los gimnasios correspondientes (usando referencias del estado global)
-    for gym in game_state.gyms:
-        if gym.type == "Roca":
+    for gym in gs.gyms:
+        if gym.type_ == "Roca":
             gym.add_trainer(trainer_brock)
-        elif gym.type == "Agua":
+        elif gym.type_ == "Agua":
             gym.add_trainer(trainer_misty)
-        elif gym.type == "Eléctrico":
+        elif gym.type_ == "Eléctrico":
             gym.add_trainer(trainer_ltsurge)
-        elif gym.type == "Lucha":
+        elif gym.type_ == "Lucha":
             gym.add_trainer(trainer_sabrina)
     
     # Agregar todos los entrenadores al estado global
-    game_state.trainers.extend(new_trainers)
-    game_state.user = trainer_user
+    gs.trainers.extend(new_trainers)
+    gs.user = trainer_user
 
 def load_trainers_pokemons():
     """
@@ -52,17 +57,17 @@ def load_trainers_pokemons():
     weights = [1, 1/2, 1/4, 1/8, 1/16, 1/32]
     options = list(range(1, 7))
 
-    for trainer in game_state.trainers:
+    for trainer in gs.trainers:
         if trainer.name == "User":
             continue  # No asignamos Pokémon al "User"
         
         # Determinar el tipo preferido en función del gimnasio del entrenador (si tiene)
-        preferred_type = trainer.gym.type if trainer.gym is not None else None
+        preferred_type = trainer.gym.type_ if trainer.gym is not None else None
         
         num_pokemons = random.choices(options, weights=weights, k=1)[0]
         for _ in range(num_pokemons):
             # Se pasa el preferred_type si está disponible, de lo contrario se genera sin sesgo
-            pokemon = create_pokemons.create_random_pokemon(preferred_type=preferred_type)
+            pokemon = cp.create_random_pokemon(preferred_type=preferred_type)
             trainer.add_pokemon(pokemon)
 
 def load_user_pokemon(species):
@@ -73,13 +78,14 @@ def load_user_pokemon(species):
       - species (str): la especie del Pokémon a generar.
     """
     # Genera el Pokémon usando la función especializada
-    pokemon = create_pokemons.create_pokemon_by_species(species)
+    pokemon = cp.create_pokemon_by_species(species)
     # Asigna el Pokémon al usuario (suponiendo que game_state.user ya fue inicializado)
-    game_state.user.add_pokemon(pokemon)
+    gs.user.add_pokemon(pokemon)
 
 def load_data():
     # Función central para precargar todos los datos del juego.
-    load_gyms()
+    load_obstacles()
+    load_locations()
     load_trainers()
     load_trainers_pokemons()
     load_user_pokemon("Pikachu")
